dea53dde94b5ed095628caae3442cc30e5633082
f3gomes <fgomesdeluna@gmail.com>
2025-01-13 15:17:24 -0300

    feat: add sign-in integration

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isAuth = request.cookies.get("gtxp");
+
+  if (isAuth && request.nextUrl.pathname === "/sign-in") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!isAuth && request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/sign-in"],
+};
