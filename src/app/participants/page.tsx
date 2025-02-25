e297b2035a86bc82178053f273c00b035c196e78
f3gomes <fgomesdeluna@gmail.com>
2025-02-25 14:32:41 -0300

    feat: include participant list loading

--- src/app/participants/page.tsx
+++ src/app/participants/page.tsx
@@ -1,26 +1,11 @@
-"use client";
-
-import { listUsers } from "@/actions/listUsers";
 import AsideLeft from "@/components/custom/aside-left";
 import AsideRight from "@/components/custom/aside-right";
 import Header from "@/components/custom/header";
 import ParticipantsList from "@/components/custom/participants-list";
 
 import { sideIcons, socialIcons } from "@/components/data/list";
-import { useEffect, useState } from "react";
 
 export default function ParticipantsPage() {
-  const [participantsList, setParticipantsList] = useState([]);
-
-  useEffect(() => {
-    const fetchList = async () => {
-      const list: any = await listUsers();
-      setParticipantsList(list?.data?.updatedList);
-    };
-
-    fetchList();
-  }, []);
-
   return (
     <>
       <div className="flex min-h-screen w-full flex-col bg-gray-50">
@@ -29,7 +14,7 @@ export default function ParticipantsPage() {
         <main className="mx-auto flex w-full flex-grow flex-col justify-center gap-2 p-4 xl:flex-row xl:items-start">
           <AsideLeft sideIcons={sideIcons} socialIcons={socialIcons} />
 
-          <ParticipantsList participants={participantsList} />
+          <ParticipantsList />
 
           <AsideRight />
         </main>
--- src/components/custom/participants-card.tsx
+++ src/components/custom/participants-card.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Card, CardContent } from "../ui/card";
 import { useState } from "react";
+import Link from "next/link";
 
 export interface ParticipantCardProps {
+  index: number;
   name: string;
   role: string;
   image: string;
@@ -10,6 +12,7 @@ export interface ParticipantCardProps {
 }
 
 export default function ParticipantCard({
+  index,
   name,
   role,
   image,
@@ -19,30 +22,32 @@ export default function ParticipantCard({
 
   return (
     <>
-      <Card className="flex h-[120px] w-[310px] items-center justify-center gap-2 rounded-[21px] bg-[#F3F3F3] p-2">
-        <div className="items-center justify-center">
-          {!error ? (
-            <Image
-              width={200}
-              height={200}
-              alt="Imagem de Perfil"
-              onError={() => setError(true)}
-              className="h-[100px] w-[100px] rounded-full object-cover"
-              src={`${process.env.NEXT_PUBLIC_URL_ENDPOINT}/tr:w-150,h-150,fo-face/${image}`}
-            />
-          ) : (
-            <span className="flex h-[100px] w-[100px] items-center justify-center rounded-full bg-slate-200 text-xs text-gray-500">
-              Sem Foto
-            </span>
-          )}
-        </div>
+      <Link href={`/participants/${index}`}>
+        <Card className="flex h-[120px] w-[310px] items-center justify-center gap-2 rounded-[21px] bg-[#F3F3F3] p-2">
+          <div className="items-center justify-center">
+            {!error ? (
+              <Image
+                width={200}
+                height={200}
+                alt="Imagem de Perfil"
+                onError={() => setError(true)}
+                className="h-[100px] w-[100px] rounded-full object-cover"
+                src={`${process.env.NEXT_PUBLIC_URL_ENDPOINT}/tr:w-150,h-150,fo-face${image}`}
+              />
+            ) : (
+              <span className="flex h-[100px] w-[100px] items-center justify-center rounded-full bg-slate-200 text-xs text-gray-500">
+                Sem Foto
+              </span>
+            )}
+          </div>
 
-        <CardContent className="flex h-[100px] w-[200px] flex-col items-start justify-center gap-1 rounded-[15px] bg-white p-2 text-left text-sm">
-          <span className="text-base font-bold">{name}</span>
-          <span className="text-sm">{role}</span>
-          <span>{activities}</span>
-        </CardContent>
-      </Card>
+          <CardContent className="flex h-[100px] w-[200px] flex-col items-start justify-center gap-1 rounded-[15px] bg-white p-2 text-left text-sm">
+            <span className="text-base font-bold">{name}</span>
+            <span className="text-sm">{role}</span>
+            <span>{activities}</span>
+          </CardContent>
+        </Card>
+      </Link>
     </>
   );
 }
