97a17d4a360a6d013a5ce653988407c24b10434e
Japoleca <vitorsampaiond057@gmail.com>
2025-04-06 10:02:25 -0300

    feat: agenda segundo dia + patrocinio

--- src/components/custom/aside-right.tsx
+++ src/components/custom/aside-right.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
 export default function AsideRight() {
   return (
-    <aside className="h-[600px] min-w-[330px] rounded-2xl bg-white p-4 shadow-md">
-      <hr className="my-4 border-gray-300" />
-      <div className="flex items-center gap-2 font-semibold text-gray-400">
+    <aside className="sticky top-4 w-[330px] rounded-2xl bg-white p-4 shadow-md">
+      {/* Banner centralizado */}
+      <div className="flex flex-col items-center">
+        <Image
+          width={250}
+          height={78}
+          src="/assets/Patrocinio.png"
+          alt="Congresso Logo"
+          className="w-[300px] object-contain"
+        />
+        <hr className="my-4 w-full border-gray-300" />
+      </div>
+      <div className="flex items-start gap-2 font-semibold text-gray-400">
         <span>Patrocinado</span>
       </div>
     </aside>
